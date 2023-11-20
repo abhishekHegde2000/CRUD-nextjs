3de806e25b9120d2063d6e4735781d03d77ba689
abhishek Hegde <ahegde2021@gmail.com>
2023-11-20 22:21:44 +0530

    table created

--- app/page.js
+++ app/page.js
@@ -1,6 +1,16 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 
 
 export default function Home() {
@@ -22,7 +32,32 @@ export default function Home() {
           <Button className="transition duration-500 ease-in-out hover:bg-blue-500 active:bg-blue-700 cursor-pointer">Add ToDo</Button>
         </form>
 
-        
+        <Table>
+        <TableCaption>A list of ToDos</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>1</TableCell>
+            <TableCell>Mark</TableCell>
+            <TableCell>Otto</TableCell>
+            <TableCell>@mdo</TableCell>
+            <TableCell className="space-x-2">
+              <Button className="transition duration-500 ease-in-out hover:bg-blue-500 active:bg-blue-950 cursor-pointer">Delete</Button>
+              <Button className="transition duration-500 ease-in-out hover:bg-blue-500 active:bg-blue-950 cursor-pointer">Update</Button>  
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+
         </section>
 
     </>
